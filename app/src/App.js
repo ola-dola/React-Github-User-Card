4866e8c77bfa261ddf7c3b864d5f99df9c1e49c7
oredola <oredolaolamide@gmail.com>
2019-12-06 16:35:56 +0100

    wip: refactor into class components

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,40 @@ import axios from "axios";
 import "./App.css";
 import UserList from "./Components/UserList";
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/ola-dola")
+      .then(response => {
+        console.log(response.data);
+        this.setState(currentState => ({
+          userData: [...currentState.userData, response.data]
+        }));
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
+
+  render() {
+    const { userData } = this.state;
+
+    return (
+      <div className="App">
+        <h2>Github Data</h2>
+        <UserList userData={userData} />
+      </div>
+    );
+  }
+}
+
 function AppOld() {
   const [userData, setUserData] = useState([]);
 
